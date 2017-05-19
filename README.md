af791846fd1c2096d50faad9609571ca51230520
Sion Williams <sion@byteshifter.io>
2017-05-19 09:20:20 +0100

    Test commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Install the pre-commit hook
+
+This is required every time we clone the repository.
+
+`ln -s ../../pre-commit.sh .git/hooks/pre-commit`
---
+++ pre-commit.sh
@@ -0,0 +1,17 @@
+# stash unstaged changes so that they arent included in the 
+# test below
+STASH_NAME="pre-commit-$(date +%s)"
+git stash save -q --keep-index $STASH_NAME
+
+# Test prospective commit
+echo "Running build process"
+# ./run default
+# RESULT=$?
+
+STASHES=$(git stash list)
+if [[ $STASHES == "$STASH_NAME" ]]; then
+  git stash pop -q
+fi
+
+[ $RESULT -ne 0 ] && exit 1
+exit 0
